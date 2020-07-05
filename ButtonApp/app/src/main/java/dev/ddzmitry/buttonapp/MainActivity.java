36e4fd1cd5bfba750b5a8cc9aa06931d826b38af
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2020-07-05 13:23:07 -0400

    Starterd

---
+++ ButtonApp/app/src/main/java/dev/ddzmitry/buttonapp/MainActivity.java
@@ -0,0 +1,23 @@
+package dev.ddzmitry.buttonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+        System.out.println(view);
+        Log.i("Info","clickFunciton was clicked");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
