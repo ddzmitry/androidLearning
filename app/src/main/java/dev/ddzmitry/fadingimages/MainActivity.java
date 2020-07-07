820d95ee02e3d4d06d1cc0a5f3163672e84d86d2
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2020-07-07 08:37:32 -0400

    animations

--- app/src/main/java/dev/ddzmitry/fadingimages/MainActivity.java
+++ app/src/main/java/dev/ddzmitry/fadingimages/MainActivity.java
@@ -8,21 +8,34 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void bringbart(View view){
+        Log.i("info","App is loaded");
+        ImageView imageBart = findViewById(R.id.imageViewBart);
+        imageBart.animate().translationX(1000).rotation(1800).scaleX(1f).scaleY(1f).setDuration(2000);
+
+    }
     public void fadeImage(View view){
 
-
-
         ImageView imageBart = findViewById(R.id.imageViewBart);
-        ImageView imageHomer = findViewById(R.id.imageViewHomer);
-        if(imageBart.getAlpha() == 0 ){
-            imageHomer.animate().alpha(0).setDuration(2000);
-            imageBart.animate().alpha(1).setDuration(2000);
-        } else{
-            imageBart.animate().alpha(0).setDuration(2000);
-            imageHomer.animate().alpha(1).setDuration(2000);
-        }
+        // Move down
+//        imageBart.animate().translationYBy(1000).setDuration(2000);
+        //move left negative , move right positive
+        //imageBart.animate().translationX(-1200).setDuration(2000);
+        // rotate and disapear
+        // imageBart.animate().rotation(1800).alpha(0).setDuration(1000);
+
+        imageBart.animate().scaleX(0.5f).scaleY(0.5f).rotation(1800).alpha(0).setDuration(1000);
 
 
+
+//        ImageView imageHomer = findViewById(R.id.imageViewHomer);
+//        if(imageBart.getAlpha() == 0 ){
+//            imageHomer.animate().alpha(0).setDuration(2000);
+//            imageBart.animate().alpha(1).setDuration(2000);
+//        } else{
+//            imageBart.animate().alpha(0).setDuration(2000);
+//            imageHomer.animate().alpha(1).setDuration(2000);
+//        }
 
 
         Log.i("Info","Imageview Was tapped");
@@ -32,5 +45,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageBart = findViewById(R.id.imageViewBart);
+//        imageBart.animate().translationX(-1000);
+        imageBart.setX(-1000);
+        imageBart.setScaleX(0.1f);
+        imageBart.setScaleY(0.1f);
+        imageBart.animate().translationXBy(1000).rotation(3600).scaleX(1f).scaleY(1f).setDuration(2000);
     }
 }
