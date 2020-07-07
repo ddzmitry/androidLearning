9bde6bee74b00b97c3ae10612a80e5bf01a5c14b
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2020-07-06 20:32:45 -0400

    learning

--- TextFieldLearning/app/src/main/java/dev/ddzmitry/textfieldlearning/MainActivity.java
+++ TextFieldLearning/app/src/main/java/dev/ddzmitry/textfieldlearning/MainActivity.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
 
     public void clickFunction(View view){
         // Get element by id cast it as EditText
@@ -18,8 +18,11 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Info","clickFunciton was clicked");
         Log.i("username", UserNameText.getText().toString());
         Log.i("password", PaswordNameText.getText().toString());
-
         Toast.makeText(this, String.format("Hello \n %s",UserNameText.getText().toString()), Toast.LENGTH_SHORT).show();
+        ImageView catView = findViewById(R.id.imageCat);
+        catView.setImageResource(R.drawable.cat3);
+
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
